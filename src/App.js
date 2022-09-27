f9e6d5732664b478d48fd9bccf949317341cc77e
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2022-09-27 18:57:19 +0200

    Saved notes to the localStorage & added note title to the summary

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import Editor from './components/Editor';
-// import { data } from './data';
+// import { data } from'./data';
 import Split from 'react-split';
 import { nanoid } from 'nanoid';
 
@@ -19,10 +19,16 @@ export default function App() {
 	 *    into a real JS array.
 	 */
 
-	const [notes, setNotes] = React.useState([]);
+	const [notes, setNotes] = React.useState(
+		() => JSON.parse(localStorage.getItem('notes')) || []
+	);
 	const [currentNoteId, setCurrentNoteId] = React.useState(
 		(notes[0] && notes[0].id) || ''
 	);
+	
+	React.useEffect(() => {
+		localStorage.setItem('notes', JSON.stringify(notes));
+	}, [notes])
 
 	function createNewNote() {
 		const newNote = {
